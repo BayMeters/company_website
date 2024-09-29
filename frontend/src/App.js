a642c46120ac45446fb50668d76d919ddd3e757b
Rsirp0c <haorancheng@brandeis.edu>
2024-09-28 23:11:30 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Import your page components
 import Mainpage from './pages/Mainpage.js';
 import ProductCategoryPage from './pages/ProductCategoryPage.js';
-import SingleProductPage from './pages/SingleProductPage.js';
+import SingleProductPage from './pages/Singleproductpage.js';
 import Aboutpage from './pages/About.js';
 import Contactpage from './pages/Contact.js';
 import Comingsoonpage from './pages/Comingsoon.js';
